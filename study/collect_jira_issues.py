920ff5bd3af19c3bb2d3fae82bc7da1c2a954676
saucenschwinger <mopmop.tschopflop@web.de>
2022-02-14 18:26:36 +0100

    changed jira and vcs to firepad versions

--- study/collect_jira_issues.py
+++ study/collect_jira_issues.py
@@ -1,54 +1,45 @@
 import csv
 from xml.dom.minidom import Element
+import os
 import requests
 
-def get_versions_without_name(versions):
-    result = ' '.join([issue['name'].replace('Jena','') for issue in versions])
-    return result
-
-def convert_issue_to_record(issue):
-    record = {}
-    fields = issue['fields']
-    record['key'] = issue['key']
-    record['priority'] = fields['priority']['name']
-    record['created'] = fields['created']
-    record['status'] = fields['status']['name']
-    record['versions'] = get_versions_without_name(fields['versions'])
-    return record
-
-def get_records_from_project(project,page_length):
+def main(repo):
+    filename = os.path.join("..", "data", "jira_records.csv")
     jira_api_base_url = "https://issues.apache.org/jira/"
-    proj_jira_id = project.upper()
+    page_length = 500
+    proj_jira_id = repo.upper()
     start_idx = 0
-    url = (
-        jira_api_base_url
-        + f"rest/api/2/search?jql=project={proj_jira_id}+order+by+created"
-        + f"&issuetypeNames=Bug&maxResults={page_length}&"
-        + f"startAt={start_idx}&fields=id,key,priority,labels,versions,"
-        + "status,components,creator,reporter,issuetype,description,"
-        + "summary,resolutiondate,created,updated"
-    )
-    print(f"Getting data from {url}...")
-    r = requests.get(url)
-    r_dict = r.json()
-    result = []
+    total = 10**8
+    final = []
 
-    for issue in r_dict['issues']:
-        record = convert_issue_to_record(issue)
-        result.append(record)
-    return result
+    while total > start_idx:
+        url = (
+                jira_api_base_url
+                + f"rest/api/2/search?jql=project={proj_jira_id}+order+by+created"
+                + f"&issuetypeNames=Bug&maxResults={page_length}&"
+                + f"startAt={start_idx}&fields=id,key,priority,labels,versions,"
+                + "status,components,creator,reporter,issuetype,description,"
+                + "summary,resolutiondate,created,updated"
+                )
+        print(f"Getting data from {url}...")
+        r = requests.get(url)
+        r_dict = r.json()
+        start_idx += page_length
+        total = r_dict['total']
+        final.append(r_dict)
 
-def write_records_to_csv_file(filename,records):
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['key', 'priority', 'created', 'status', 'versions'])
-        for record in records:
-            writer.writerow([record['key'], record['priority'], record['created'], record['status'], record['versions']])
-def main(repo):
-    records = get_records_from_project('jena',200)
-    write_records_to_csv_file("records.csv",records)
-    pass
-
+        for piece in final:
+            for issue in piece['issues']:
+                key = issue['key']
+                priority = issue['fields']['priority']['name']
+                created = issue['fields']['created']
+                status = issue['fields']['status']['name']
+                versions = ' '.join([version['name'] for version in issue['fields']['versions']])
+                # print(key, priority, created, status, versions)
+                writer.writerow([key, priority, created, status, versions])
 
 if __name__ == "__main__":
-    main(args.repo)
+    main("camel")
